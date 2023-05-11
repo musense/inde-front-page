bce59976b3277e07ec1ca403ac914b3dbd749193
musense <musense.ad@gmail.com>
2023-05-11 15:03:30 +0800

    解決menu selected active 問題

--- src/components/Navbars/NavButton.jsx
+++ src/components/Navbars/NavButton.jsx
@@ -1,60 +1,30 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { NavItem } from "reactstrap";
 import { css } from '@emotion/css'
+import styles from './navButton.module.css'
+
 
 export default function NavButton({ category, closeMenu }) {
 
     const CategoryShowName = category.charAt(0).toUpperCase() + category.slice(1);
 
-    const style = css`
-            box-sizing: border-box;
-            text-decoration: none;
-            font-size: 23px;
-            color: var(--theme-text);
+    const { pathname } = useLocation();
 
-            &.active,
-            &:hover {
-                text-decoration: none;
-                color: var(--theme-gold);
-            }
-            @media screen and (max-width: 400px) {
+    const selectedCategory = (category) => {
+        const path = category.substring(category.lastIndexOf("/") + 1, category.length)
+        if (path === '') return 'home'
+        else return path
+    }
 
-                position: relative;
-                color: var(--theme-color);
-
-                &.active,
-                &:hover {
-                    text-decoration: none;
-                    color: var(--theme-color);
-                }
-                &.active{
-                    font-size: 2.5rem;
-                    font-family: Lobster-Regular;
-                    letter-spacing: 0.2rem;
-                }
-
-                &::after{
-                    content: "";
-                    position: absolute;
-                    display: block;
-                    background-image: url(/src/assets/img/mobile/index/jewelry.png);
-                    background-size: contain;
-                    background-repeat: no-repeat;
-                    background-position: center;
-                    width: 10vw;
-                    height: 3vw;
-                    bottom: -1.6rem;
-                    left: 50%;
-                    transform: translateX(-50%);                
-                }
-        `
     return (
         <NavItem>
             <NavLink
                 onClick={closeMenu}
                 to={`${category === 'home' ? '' : `/c/${category}`}`}
-                className={style}
+                className={`${styles['navButton']} ${selectedCategory(pathname) === category
+                        ? styles['active']
+                        : ''}`}
             >
                 {CategoryShowName}
             </NavLink>
